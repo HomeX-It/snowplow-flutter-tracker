fb89a5838424f4e25f8751f11c8932550dad3705
Jason Rai <jrai@homex.com>
2021-03-09 08:41:36 -0500

    remove builder pattern from ConsentDocument

--- lib/src/events/consent_document.dart
+++ lib/src/events/consent_document.dart
@@ -12,11 +12,13 @@ class ConsentDocument {
   /// [documentDescription] The consent document description.
   final String documentDescription;
 
-  ConsentDocument._builder(ConsentDocumentBuilder builder)
-      : documentId = builder._documentId,
-        documentVersion = builder._documentVersion,
-        documentName = builder._documentName,
-        documentDescription = builder._documentDescription;
+  /// Create a [ConsentDocument]
+  ConsentDocument({
+    this.documentId,
+    this.documentVersion,
+    this.documentName,
+    this.documentDescription,
+  });
 
   /// Converts the consent document object to JSON.
   Map<String, Object> toMap() {
@@ -26,42 +28,5 @@ class ConsentDocument {
       'documentName': documentName,
       'documentDescription': documentDescription,
     };
-  }
-}
-
-/// The protocol for building consent documents.
-class ConsentDocumentBuilder {
-  String _documentId;
-  String _documentVersion;
-  String _documentName;
-  String _documentDescription;
-
-  /// Set the ID associated with a document that defines consent.
-  ConsentDocumentBuilder setDocumentId(String documentId) {
-    _documentId = documentId;
-    return this;
-  }
-
-  /// Set the version of the consent document.
-  ConsentDocumentBuilder setDocumentVersion(String documentVersion) {
-    _documentVersion = documentVersion;
-    return this;
-  }
-
-  /// Set the name of the consent document.
-  ConsentDocumentBuilder setDocumentName(String documentName) {
-    _documentName = documentName;
-    return this;
-  }
-
-  /// Set the description of the consent document.
-  ConsentDocumentBuilder setDocumentDescription(String documentDescription) {
-    _documentDescription = documentDescription;
-    return this;
-  }
-
-  /// Creates a consent document.
-  ConsentDocument build() {
-    return ConsentDocument._builder(this);
   }
 }
