210593fe4301f992978dc926de4e6ad14cd48b31
Daniel Peter <ohitsdaniel@users.noreply.github.com>
2021-04-15 22:18:16 +0200

    Adds SnowplowFlutterTracker.close() tests (#29)

--- test/snowplow_flutter_tracker_test.dart
+++ test/snowplow_flutter_tracker_test.dart
@@ -2,6 +2,11 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:snowplow_flutter_tracker/snowplow_flutter_tracker.dart';
 
+/// ⚠️ Important ⚠️: For now, isInitialised is a static field.
+/// Therefore, the whole test suite needs to run in order,
+/// so that the flag has the correct value at the time the test is run.
+/// Make sure to not change the order of [initialize] and [close] tests
+/// or else this test suite will break!
 void main() {
   const channel = MethodChannel('snowplow_flutter_tracker');
   final tracker = Tracker(
@@ -31,6 +36,7 @@ void main() {
         await sut.initialize();
 
         expect(initialiseWasCalled, equals(true));
+        expect(SnowplowFlutterTracker.isInitialized, equals(true));
       },
     );
 
@@ -50,11 +56,55 @@ void main() {
           await sut.initialize();
         } on SnowplowFlutterTrackerInitialisationError {
           expect(initialiseWasCalled, equals(false));
+          expect(SnowplowFlutterTracker.isInitialized, equals(true));
         } catch (_) {
           fail(
             'Expected SnowplowFlutterTrackerInitialisationError to be thrown',
           );
         }
+      },
+    );
+  });
+
+  group('[close]', () {
+    test(
+      "If tracker is initialised, calls 'close' on platform channel",
+      () async {
+        var closeWasCalled = false;
+
+        final sut = SnowplowFlutterTracker(tracker);
+
+        channel.setMockMethodCallHandler((MethodCall methodCall) async {
+          closeWasCalled = true;
+
+          expect(methodCall.method, equals('close'));
+          return;
+        });
+
+        await sut.close();
+
+        expect(closeWasCalled, equals(true));
+        expect(SnowplowFlutterTracker.isInitialized, equals(false));
+      },
+    );
+
+    test(
+      "If tracker is not initialised, does not call 'close' on platform channel",
+      () async {
+        var closeWasCalled = false;
+
+        final sut = SnowplowFlutterTracker(tracker);
+
+        channel.setMockMethodCallHandler((MethodCall methodCall) async {
+          closeWasCalled = true;
+          expect(methodCall.method, equals('close'));
+          return;
+        });
+
+        await sut.close();
+
+        expect(closeWasCalled, equals(false));
+        expect(SnowplowFlutterTracker.isInitialized, equals(false));
       },
     );
   });
